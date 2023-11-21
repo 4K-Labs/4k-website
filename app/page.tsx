7a77ab2188713aa5b4bc0dd6cbde050042e9311d
emran <emranhi001@gmail.com>
2023-11-21 07:48:37 +0300

    refined the testimony section of the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import AboutUs from '@/components/AboutUs'
+import Testimony from '@/components/Testimony'
+
 export default function Home() {
 
   return (
     <div className="bg-gray-100">
       <AboutUs/>
+      <Testimony/>
     </div>
   )
 }
---
+++ components/Testimony.tsx
@@ -0,0 +1,165 @@
+'use client'
+
+import React, { Component } from "react";
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Typography,
+    Avatar,
+  } from "@material-tailwind/react";
+
+const Testimony = ()=>{
+
+    const testimonials = [
+        {
+          id:0,
+          name: "John Doe",
+          image: "/img/person-1.jpg",
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          title: "Frontend Lead @ Google"
+        },
+        {
+          id:1,
+          name: "Jane Smith",
+          image: "/img/person-1.jpg",
+          content: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          title: "Frontend Lead @ Google"
+        },
+        {
+          id:2,
+          name: "Bob Johnson",
+          image: "/img/person-1.jpg",
+          content: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+          title: "Frontend Lead @ Google"
+        },
+        {
+          id:3,
+          name: "Alice Brown",
+          image: "/img/person-1.jpg",
+          content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          title: "Frontend Lead @ Google"
+        },
+        {
+          id:4,
+          name: "Chris Green",
+          image: "/img/person-1.jpg",
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          title: "Frontend Lead @ Google"
+        },
+      ];
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        initialSlide: 1,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        nextArrow: <NextArrow className={undefined} style={undefined} onClick={undefined} />,
+        prevArrow: <PrevArrow className={undefined} style={undefined} onClick={undefined} />,
+        responsive: [
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              
+            }
+          },
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+        };  
+      
+    return (
+    <div className='w-4/5 m-auto'>
+        <div className='m-4'>
+          <Slider {...settings}>
+            {testimonials.map((testimony)=>(
+                <div key={testimony.id} className=''>
+                    <Card color="transparent" shadow={false} className="w-64">
+                        <CardHeader
+                        color="transparent"
+                        floated={false}
+                        shadow={false}
+                        className="mx-0 flex items-center gap-4 pt-0 pb-8"
+                        >
+                          <Avatar
+                              size="lg"
+                              variant="circular"
+                              src={testimony.image}
+                              alt={testimony.name}
+                          />
+                          <div className="flex w-full flex-col gap-0.5">
+                              <div className="flex items-center justify-between">
+                              <Typography variant="h5" color="blue-gray">
+                                  {testimony.name}
+                              </Typography>
+                              </div>
+                              <Typography color="blue-gray">{testimony.title}</Typography>
+                          </div>
+                        </CardHeader>
+                        <CardBody className="mb-6 p-0">
+                            <Typography>{testimony.content}</Typography>
+                        </CardBody>
+                    </Card>
+                </div>
+            ))}
+          </Slider>
+        </div>
+
+    </div>
+    )
+}
+
+export default Testimony
+  
+function StarIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="h-5 w-5 text-yellow-700"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+   
+function NextArrow(props: { className: any; style: any; onClick: any; }) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "gray" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function PrevArrow(props: { className: any; style: any; onClick: any; }) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "gray" }}
+      onClick={onClick}
+    />
+  );
+}
