6b2870579a8e8b61266f101f4eb7fcad8d6ea767
Tamiru-Alemnew <tamirualemnew33@gmail.com>
2024-02-05 13:10:26 +0300

    make design change

--- app/components/About/MissionAndVision.tsx
+++ app/components/About/MissionAndVision.tsx
@@ -2,13 +2,22 @@
 import Image from "next/image";
 import Link from "next/link";
 
+interface SectionWithImageProps {
+  title: string;
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+  reverse?: boolean;
+}
 
-function SectionWithImage({ title, description, imageSrc, imageAlt, reverse }) {
+function SectionWithImage({ title, description, imageSrc, imageAlt, reverse }: SectionWithImageProps) {
   return (
     <div
-      className={`flex ${
-        reverse ? "flex-col-reverse" : "flex-col"
-      } md:flex-row md:px-12 p-3 items-center`}
+      className={`flex flex-col ${
+        reverse
+          ? " md:flex-row-reverse"
+          : "flex-col md:flex-row"
+      }  md:px-12 p-3 items-center`}
     >
       <div className="flex-1 md:p-12 z-10 rounded-e-3xl">
         <h1 className="text-lg mt-4 md:text-4xl mb-4 font-semibold">{title}</h1>
@@ -35,31 +44,12 @@ export default function Mission() {
 
     return (
       <div className="">
-        <div className="flex flex-col md:flex-row md:px-12 p-3 md:-mb-20 items-center">
-          <div className="flex-1 rounded-2xl border-2 border-red-500 -ml-1 transform transition duration-500 ease-in-out hover:scale-110 hover:z-20">
-            <Image
-              alt={"4klabs"}
-              src={"/images/4klabs.png"}
-              width={300}
-              height={400}
-              className=" m-auto"
-            />
-          </div>
-          <div className="flex-1 md:p-12 md:pb-20 z-10 rounded-e-3xl">
-            <h1 className="text-lg mt-4 md:text-4xl mb-4 font-semibold">
-              4K Bots
-            </h1>
-            <div>
-              <p className="text-sm">
-                Is the division of 4K Labs, focusing on robotics and Artificial
-                Intelligence. In this focus area members of 4K Labs work on
-                projects such as; controlling robots for playing football,
-                programming robots to complete autonomous tasks, and Autonomous
-                cars.
-              </p>
-            </div>
-          </div>
-        </div>
+        <SectionWithImage
+          title="4K Bots"
+          description="Is the division of 4K Labs, focusing on robotics and Artificial Intelligence. In this focus area members of 4K Labs work on projects such as; controlling robots for playing football, programming robots to complete autonomous tasks, and Autonomous cars."
+          imageSrc="/images/4klabs.png"
+          imageAlt="4klabs"
+        />
         <SectionWithImage
           title="4K Things"
           description="In this age of technology, embedded systems are everywhere in our daily life starting from smart watches to the smart cars we drive. 4K Labs is interested in researching and developing solutions to existing problems that seek hardware solutions. 4K Things is a specialization of 4K Labs that focuses on embedded systems, IoT, and Tiny Machine Learning. Some of the projects done in this specialization are Smart mirrors, home automation, smart power meter and hardware-based games."
@@ -67,6 +57,14 @@ export default function Mission() {
           imageAlt="4klabs"
           reverse
         />
+
+        <SectionWithImage
+          title="4K Dev"
+          description="4K Dev is a specialization of 4K Labs that focuses on developing software solutions to existing problems that have been identified. The teams in 4K dev are the contact point when there is interest in bringing a hardware solution to the cloud. 4K Dev specialization was able to develop software and websites such as the 4K Labs website, class scheduling app, and agile working platform for the team."
+          imageSrc="/images/4klabs.png"
+          imageAlt="4klabs"
+        />
+
         <div className="text-center m-20">
           <Link href="/contactus">
             <button
--- app/components/About/WhoAreWe.tsx
+++ app/components/About/WhoAreWe.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function WhoAreWe() {
     return (
       <div className="flex flex-col-reverse md:flex-row  p-3">
-        <div className="flex-1 bg-gray-100 p-6 md:p-12 z-10 rounded-e-3xl">
+        <div className="flex-1 bg-gray-100 p-6 md:p-12 z-10 ">
           <h1 className="text-lg md:text-4xl mb-4  font-semibold text-center">
-            Who Are We
+            Who we are
           </h1>
           <div>
             <p>
@@ -20,15 +20,12 @@ export default function WhoAreWe() {
             </p>
           </div>
         </div>
-        <div className="flex-1 -ml-4 transform transition duration-500 hover:scale-110 hover:z-20">
-          <Image
+        <Image
             alt={"4klabs"}
             src={"/images/gathering.jpg"}
-            width={600}
+            width={500}
             height={500}
-            className="rounded-2xl"
           />
-        </div>
       </div>
     );
 }
